644f6f65e1693ade18e70f6cd84840541233484e
PolinaZar <zaruczkaya.polina@mail.ru>
2017-06-08 20:37:30 +0400

    Add files via upload

--- exp_syst_calc.py
+++ exp_syst_calc.py
@@ -1,44 +1,46 @@
 def calc(x):
-    ver_var_Samsung Galaxy A3 = {0:0.1, 1:1, 2:1, 5:1, 7:0}
-    ver_var_Samsung Galaxy A5 = {0:0.1, 1:1, 3:1, 4:1, 7:0}
-    ver_var_Samsung Galaxy S7 = {0:0.5, 1:1, 7:0.8}
-    ver_var_Samsung Galaxy S8 = {0:0.3, 1:1, 7:0.7}
-    ver_var_iPhone6S = {0:0.1, 1:1, 8:0.6, 7:0}
-    ver_var_iPhone7 = {0:0.1, 1:1, 6:1, 7:0.6}
-    ver_var_Lenovo Vibe K5 Note= {0:0.1, 1:1, 6:1, 7:0.5}
-    ver_var_Lenovo K6 Note  = {0:0.1, 1:1, 6:1, 7:0.4}
-    ver_var_Lenovo Moto X Style = {0:0.1, 1:1, 7:0}
-    ver_var_Lenovo Moto Z = {0:0.1, 1:1, 6:1, 7:0.3}
+    ver_var_Sams_A3 = {0:0.1, 1:1, 2:0, 3:0, 6:0, 7:1}
+    ver_var_Sams_A5 = {0:0.1, 1:1, 2:0, 3:0, 6:1, 7:1}
+    ver_var_Sams_S7 = {0:0.5, 1:1, 2:0, 3:0, 6:0, 7:1}
+    ver_var_Sams_S8 = {0:0.3, 1:1, 2:0, 3:0, 6:1, 7:1}
+    ver_var_Iphone_6S = {0:0.1, 1:0, 2:1, 3:0, 6:0, 8:1}
+    ver_var_Iphone_7 = {0:0.1, 1:0, 2:1, 3:0, 6:1, 8:1}
+    ver_var_Len_K5 = {0:0.1, 1:0, 2:0, 3:1, 6:0, 7:1}
+    ver_var_Len_K6 = {0:0.1, 1:0, 2:0, 3:1, 6:1, 7:1}
+    ver_var_Len_X = {0:0.1, 1:0, 2:0, 3:1, 6:0, 7:1}
+    ver_var_Len_Z = {0:0.1, 1:0, 2:0, 3:1, 6:1, 7:1}
     for i in range(0,len(x)-1):
-        for j in range(0,len(ver_var_Samsung Galaxy A3)-1):
-            if i==list(ver_var_Samsung Galaxy A3.keys())[j]:
-                 Samsung Galaxy A3 = x[i]*list(ver_var_Samsung Galaxy A3.values())[j]
-        for k in range(0,len(ver_var_Samsung Galaxy A5)-1):
-            if i==list(ver_var_Samsung Galaxy A5.keys())[k]:
-                Samsung Galaxy A5 = x[i]*list(ver_var_Samsung Galaxy A5.values())[k]
-        for l in range(0,len(ver_var_Samsung Galaxy S7)-1):
-            if i==list(ver_var_Samsung Galaxy S7.keys())[l]:
-                Samsung Galaxy S7 = x[i]*list(ver_var_Samsung Galaxy S7.values())[l]
-        for m in range(0,len(ver_var_Samsung Galaxy S8)-1):
-            if i==list(ver_var_Samsung Galaxy S8.keys())[m]:
-                Samsung Galaxy S8 = x[i]*list(ver_var_Samsung Galaxy S8.values())[m]
-        for n in range(0,len(ver_var_iPhone6S)-1):
-            if i==list(ver_var_iPhone6S.keys())[n]:
-                iPhone6S = x[i]*list(ver_var_iPhone6S.values())[n]
-        for o in range(0,len(ver_var_iPhone7)-1):
-            if i==list(ver_var_iPhone7.keys())[o]:
-                iPhone7 = x[i]*list(ver_var_iPhone7.values())[o]
-        for p in range(0,len(ver_var_Lenovo Vibe K5 Note)-1):
-            if i==list(ver_var_Lenovo Vibe K5 Note.keys())[p]:
-                Lenovo Vibe K5 Note = x[i]*list(ver_var_Lenovo Vibe K5 Note.values())[p]
-        for q in range(0,len(ver_var_Lenovo K6 Note)-1):
-            if i==list(ver_var_Lenovo K6 Note.keys())[q]:
-                Lenovo K6 Note = x[i]*list(ver_var_Lenovo K6 Note.values())[q]
-        for r in range(0,len(ver_var_Lenovo Moto X Style)-1):
-            if i==list(ver_var_Lenovo Moto X Style.keys())[r]:
-                Lenovo Moto X Style = x[i]*list(ver_var_Lenovo Moto X Style.values())[r]
-        for s in range(0,len(ver_var_Lenovo Moto Z)-1):
-            if i==list(ver_var_Lenovo Moto Z.keys())[s]:
-                Lenovo Moto Z = x[i]*list(ver_var_Lenovo Moto Z.values())[s]
-    y=[str(Samsung Galaxy A3),str(Samsung Galaxy A5),str(Samsung Galaxy S7),str(Samsung Galaxy S8),str(iPhone6S),str(iPhone7),str(Lenovo Vibe K5 Note),str(Lenovo K6 Note),str(Lenovo Moto X Style),str(Lenovo Moto Z)]
+        for j in range(0,len(ver_var_Sams_A3)-1):
+            if i==list(ver_var_Sams_A3.keys())[j]:
+                Sams_A3 = x[i]*list(ver_var_Sams_A3.values())[j]
+        for k in range(0,len(ver_var_Sams_A5)-1):
+            if i==list(ver_var_Sams_A5.keys())[k]:
+                Sams_A5 = x[i]*list(ver_var_Sams_A5.values())[k]
+        for l in range(0,len(ver_var_Sams_S7)-1):
+            if i==list(ver_var_Sams_S7.keys())[l]:
+                Sams_S7 = x[i]*list(ver_var_Sams_S7.values())[l]
+        for m in range(0,len(ver_var_Sams_S8)-1):
+            if i==list(ver_var_Sams_S8.keys())[m]:
+                Sams_S8 = x[i]*list(ver_var_Sams_S8.values())[m]
+        for n in range(0,len(ver_var_Iphone_6S)-1):
+            if i==list(ver_var_Iphone_6S.keys())[n]:
+                Iphone_6S = x[i]*list(ver_var_Iphone_6S.values())[n]
+        for o in range(0,len(ver_var_Iphone_7)-1):
+            if i==list(ver_var_Iphone_7.keys())[o]:
+                Iphone_7 = x[i]*list(ver_var_Iphone_7.values())[o]
+        for p in range(0,len(ver_var_Len_K5)-1):
+            if i==list(ver_var_Len_K5.keys())[p]:
+                Len_K5 = x[i]*list(ver_var_Len_K5.values())[p]
+        for q in range(0,len(ver_var_Len_K6)-1):
+            if i==list(ver_var_Len_K6.keys())[q]:
+                Len_K6 = x[i]*list(ver_var_Len_K6.values())[q]
+        for r in range(0,len(ver_var_Len_X)-1):
+            if i==list(ver_var_Len_X.keys())[r]:
+                Len_X = x[i]*list(ver_var_Len_X.values())[r]
+        for s in range(0,len(ver_var_Len_Z)-1):
+            if i==list(ver_var__Len_Z.keys())[s]:
+                Len_Z = x[i]*list(ver_var_Len_Z.values())[s]
+    y=[str(Sams_A3),str(Sams_A5),str(Sams_S7),str(Sams_S8),str(Iphone_6S),str(Iphone_7),str(Len_K5),str(Len_K6),str(Len_X),str(Len_Z)]
     return y
+Contact GitHub API Training Shop Blog About
+© 2017 GitHub, Inc. Terms Privacy Security Status Help
